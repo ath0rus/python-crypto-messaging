b639c2e8f9ae7b673d215fe7e7b15b5668681694
ath0rus <65640442+ath0rus@users.noreply.github.com>
2021-10-12 14:52:17 +1000

    Update decrypt.py

--- V1/decrypt.py
+++ V1/decrypt.py
@@ -3,12 +3,12 @@ from cryptography.fernet import Fernet
 
 s = socket.socket()
 port = 5050
-ip = '172.16.36.8'
+ip = #replace with server ip
 s.connect((ip, port))
 
-# with open("Key.key", "rb") as r:
-#     key = r.read()
-#     r.close()
+with open("Key.key", "rb") as r:
+    key = r.read()
+    r.close()
 
 key = b'3MTnubxhjkBA3sW8M_mvzahIXTVtrgKudUi0iqAl4hk='
 
