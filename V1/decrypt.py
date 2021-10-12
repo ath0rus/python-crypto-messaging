e296eb0112ee2680c267dac2a965c25328523ab6
ath0rus <65640442+ath0rus@users.noreply.github.com>
2021-10-12 14:52:30 +1000

    Update decrypt.py

--- V1/decrypt.py
+++ V1/decrypt.py
@@ -10,8 +10,6 @@ with open("Key.key", "rb") as r:
     key = r.read()
     r.close()
 
-key = b'3MTnubxhjkBA3sW8M_mvzahIXTVtrgKudUi0iqAl4hk='
-
 f = Fernet(key)
 dmsg = f.decrypt(s.recv(1024))
 print(dmsg.decode('utf-8'))
